9c19a46faf084ba9b31011fde74930956b6044e3
telecoda <robbaines@gmail.com>
2018-02-16 20:45:15 +0000

    Committing before pull test

--- ttserver/domain/rebuild.go
+++ ttserver/domain/rebuild.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -15,19 +16,21 @@ func (s *server) Rebuild(ctx context.Context, req *proto.RebuildRequest) (*proto
 
 	resp := &proto.RebuildResponse{}
 
+	log.Printf("Rebuilding ttserver")
 	// change dir
 	goPath := os.Getenv("GOPATH")
 	if goPath == "" {
 		return nil, fmt.Errorf("$GOPATH not found")
 	}
 
-	path := filepath.Join(goPath, "github.com", "telecoda", "teletrada", "ttserver")
+	path := filepath.Join(goPath, "src", "github.com", "telecoda", "teletrada", "ttserver")
 
 	if err := os.Chdir(path); err != nil {
 		return nil, fmt.Errorf("Failed to change to directory: %s - %s", path, err)
 	}
 
 	// pull code
+	log.Printf("Pulling latest code...")
 
 	// Untar the new config into the new directory
 	cmd := exec.Command("git", "pull", "origin", "master")
@@ -37,6 +40,7 @@ func (s *server) Rebuild(ctx context.Context, req *proto.RebuildRequest) (*proto
 	}
 
 	// recompile
+	log.Printf("Compiling code...")
 
 	// return resp
 
